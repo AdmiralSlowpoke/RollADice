b3a583c89946b1d219e194bb7fe28763ab5bc2d8
Gentares <109409758+Gentares@users.noreply.github.com>
2022-07-17 22:07:24 +0500

    Update(ecce)

--- Assets/Enemies/Slime_AI.cs
+++ Assets/Enemies/Slime_AI.cs
@@ -12,9 +12,11 @@ public class Slime_AI : MonoBehaviour
     public float AtkInterval;
     private float cooldown;
     public int damage;
+    public Animator anim;
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,15 +29,18 @@ public class Slime_AI : MonoBehaviour
             {
                 if (cooldown >= AtkInterval)
                 {
+                    anim.SetTrigger("Attack");
                     Target.GetComponent<Health>().DamageMe(damage);
                     cooldown = 0;
                 }
                 nav.enabled = false;
+                anim.SetBool("Walk", false);
             }
             else
             {
                 nav.enabled = true;
                 nav.SetDestination(Target.position);
+                anim.SetBool("Walk", true);
             }
         }
         if (cooldown <= AtkInterval)
